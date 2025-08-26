68705e6de12c6856db62f1cae93809db8e4b559e
saramohamed512 <saramohamed12656@gmail.com>
2025-08-26 18:13:29 +0200

    add assignment project

---
+++ LINQSession1Ass/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LINQSession1Ass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
